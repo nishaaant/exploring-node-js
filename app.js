b3af970d121260d9341a3d7b41cb17f4a4f44f5c
nishaaant <nishaaant1608@gmail.com>
2024-12-21 23:48:09 +0530

    day 2

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ var a = 20;
 var b = 100;
 // console.log("Nishant Rajput")
 // require("./xyz.js")
-const {calculateSum,calculatedivide,calculatemultiply,calculatesubtract} =require("./calculate")
+import { calculateSum, calculatedivide, calculatemultiply, calculatesubtract } from "./calculate";
 
 console.log("Sum = "+calculateSum(a,b))
 console.log("subtract = "+calculatesubtract(a,b))
 console.log("multiply = "+calculatemultiply(a,b))
-console.log("divide = "+calculatedivide(a,b))
+console.log("divide = "+
+    calculatedivide(a,b))
 // console.log(globalThis)
--- xyz.js
+++ xyz.js
@@ -1,1 +1,23 @@
 console.log("xyz module executed");
+
+const crypto = require("crypto");
+
+var a = 2 ;
+var b = 3 ;
+
+crypto.pbkdf2Sync("password", "salt" , 5000000 , 50, "sha512");
+console.log("First Key is Generated");
+
+crypto.pbkdf2("password", "salt" , 5000000 , 50, "sha512", (err, key) => {
+    console.log("Second Key is Generated");
+
+});
+
+function calculatemultiply(a,b){
+
+    const multiply = a*b;
+    console.log(multiply);
+    
+}
+
+calculatemultiply(a,b);
